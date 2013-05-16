576f61c59fc94ab07d34e8b673054a8e9eaf4824
Takenori Takaki <takenory@gmail.com>
2013-05-17 06:29:49 +0900

    first original content

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,7 @@
-# All files in the 'lib' directory will be loaded
-# before nanoc starts compiling.
+include Nanoc3::Helpers::Blogging
+include Nanoc3::Helpers::Rendering
+include Nanoc3::Helpers::LinkTo
+
+def truncate_before_more(content)
+  content.split('<!--MORE-->').first
+end
