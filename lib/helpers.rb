3300b127e74f680cac6af9f24b5a1daaca7da2c8
Takenori Takaki <takenory@gmail.com>
2013-05-21 02:23:28 +0900

    spacified encoding

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 def summary(article)
   content = article.compiled_content.split('<!-- MORE -->').first
   "#{content}#{link_to("...続きを読む", article.path, titiel: article[:title])}"
