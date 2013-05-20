371a26d0eb843c3183e3148b4603ab1e00fa199a
Takenori Takaki <takenory@gmail.com>
2013-05-20 15:32:14 +0900

    modify 'summary' method

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,3 +1,4 @@
 def summary(article)
-  article.compiled_content.split('<!-- MORE -->').first
+  content = article.compiled_content.split('<!-- MORE -->').first
+  "#{content}#{link_to("...続きを読む", article.path, titiel: article[:title])}"
 end
